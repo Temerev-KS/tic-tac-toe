aebcc2d88b65498225df88cdac30d87d2be8df75
Kirill <temerev.ks@yandex.ru>
2021-12-20 00:56:26 +0300

    GameEngine class work in progress check_state function - almost ready

--- game_engine.py
+++ game_engine.py
@@ -24,12 +24,11 @@ class GameEngine:
     def __init__(self):
         self._players = None
         self._game_going = None
-        self.__state = np.full((3, 3), 0)
-        self.__visual_state = {'a': {1: ' ', 2: ' ', 3: ' '},
-                               'b': {1: ' ', 2: ' ', 3: ' '},
-                               'c': {1: ' ', 2: ' ', 3: ' '}}
-        self.exx_player = None
-        self.oos_player = None
+        self.__visual_state = {
+            'a': {1: ' ', 2: ' ', 3: ' '},
+            'b': {1: ' ', 2: ' ', 3: ' '},
+            'c': {1: ' ', 2: ' ', 3: ' '}
+        }
         self.__winning_states = [
             (('a', 1), ('a', 2), ('a', 3)), 
             (('b', 1), ('b', 2), ('b', 3)), 
@@ -42,8 +41,6 @@ class GameEngine:
         ]
         
     def init_empty_field(self):
-        self._game_going = True
-        self.__state = np.full((3, 3), 0)
         self.__visual_state = {'a': {1: ' ', 2: ' ', 3: ' '},
                                'b': {1: ' ', 2: ' ', 3: ' '},
                                'c': {1: ' ', 2: ' ', 3: ' '}}
@@ -61,24 +58,17 @@ class GameEngine:
     def record_move(self, column, row, value):
         self.__visual_state[column][row] = value
     
-    def check_move(self):
-        pass
-    
-    def check_state(self):
-        def get_symbol(column, row):
+    def check_state(self) -> str:
+        # Checks if there are any winning combination on the board, and returns symbol of winner combination
+        def get_symbol(column, row):  # function to get one of the symbols from the combination
             return self.__visual_state[column][row]
-        for state in self.__winning_states:
-            symbol = get_symbol(*state[0])
-            if symbol == get_symbol(*state[1]) and symbol == get_symbol(*state[2]):
-                print('kawabunga')
-    
-    def v1(self):
-        if self.__visual_state['a'][1] == 'X' and self.__visual_state['a'][2] and self.__visual_state['a'][3] == 'X':
-            return True
-        else:
-            return False
-
-    
+        for state in self.__winning_states:  # loop thru all predetermined "states" (group of coordinates)
+            symbol = get_symbol(*state[0])  # get the symbol from the first cell in the state
+            if symbol != " " or symbol is not None:  # continue only if cell is not "empty"
+                # if second and third symbols are equal - # TODO: Continue this sentence
+                if symbol == get_symbol(*state[1]) and symbol == get_symbol(*state[2]):  # if second and third symbols
+                    print('kawabunga')
+                    return symbol
     
 if __name__ == '__main__':
     engine = GameEngine()
