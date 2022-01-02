f79ac78343e1e553f6f04ad8143f5cab59278fec
Kirill Temerev <64499601+Temerev-KS@users.noreply.github.com>
2022-01-02 15:10:17 +0300

    a lot of refactoring and garbage elimination

--- game_engine.py
+++ game_engine.py
@@ -1,7 +1,6 @@
 from player import Player
 from board import Board
 import os
-# TODO: Think about passing Current message thru self and use just one short function to print that message
 # TODO: A ton of refactoring (+ standardise function and variable naming)
 
 
@@ -12,57 +11,48 @@ class GameEngine:
         self._game_on = True
         self._current_player: Player | None = None
         self._score_limit = 3
-    
-    def game_loop(self):
-        while self.is_game_on():
-            while not self.check_for_winner():
-                while not self.check_winning_combo_present():
-                    if self.board_has_empty_cells():
-                        self.change_player()
-                        self.move()
-                    else:
-                        self.result_draw()
-            self.repeat_game_input()
-        print('Good by!')
-    
-    def is_game_on(self):
-        return self._game_on
-    
-    def set_game_off(self):
-        self._game_on = False
-    
-    def repeat_game_input(self):
-        repeat_decision = str(input('Do you you want to play again? \n(press Y or N keys)\n').capitalize())
-        print(repeat_decision)
-        print(ord(repeat_decision))
-        while repeat_decision not in ('Y', 'N'):
-            print(f'Right ord = {ord("Y")}')
-            repeat_decision = str(input('Invalid input: please press "Y" key to play again or "N" key to exit\n').capitalize())
-            print(repeat_decision)
-            print(ord(repeat_decision))
-        if repeat_decision == 'Y':
-            self._game_on = True
-            self.reset_game()
-        elif repeat_decision == 'N':
-            self._game_on = False
-    
+
     @staticmethod
     def show_logo():
         with open('logo', mode='r') as lg:
             ascii_logo = lg.read()
         print(ascii_logo)
-        
+
     def display_players_score(self):
         print(f'Score:\n'
               f'{self._players[0].get_name()} : {self._players[0].show_score()}  |  '
               f'{self._players[1].get_name()} : {self._players[1].show_score()}'
               f'\n')
-    
+
     @staticmethod
     def clear_terminal():
         os.system('cls' if os.name == 'nt' else 'clear')
+
+    def game_loop(self):
+        # Main method responsible for the game
+        while self._game_on:
+            while not self.game_winner_check():
+                while not self.round_winner_check():
+                    if self.board_has_empty_cells_check():
+                        self.change_player()
+                        self.move()
+                    else:
+                        self.round_result_draw()
+            self.ask_to_repeat_game()
+        print('Good by!')
+    
+    def ask_to_repeat_game(self):
+        repeat_decision = input('Do you you want to play again? \n(press Y or N keys)\n').lower()
+        while repeat_decision not in ('y', 'n'):
+            repeat_decision = input('Invalid input: please press "Y" key to play again or "N" key to exit\n').lower()
+        if repeat_decision == 'y':
+            self._game_on = True
+            self.reset_game()
+        elif repeat_decision == 'n':
+            self._game_on = False
     
     def reset_game(self):
+        #  Reset everything to the default but keeps the name of the players
         self._board.empty()
         self._score_limit = 3
         for player in self._players:
@@ -85,43 +75,36 @@ class GameEngine:
             self._current_player = self._players[0]
         
     def move(self):
-        self.show_board()
+        self.display_board()
         print(f"\nEnter the next move")
         cell_address = self.ask_for_user_input()
         while self._board.cell_not_empty_check(*cell_address):
-            self.show_board()
+            self.display_board()
             print(f'\nSorry sell {cell_address} is not empty, please select a different one')
             cell_address = self.ask_for_user_input()
-        self._board.write_cell(*cell_address, value=self._current_player._figure)
+        self._board.write_cell(*cell_address, value=self._current_player.get_figure())
     
-    def show_board(self):
+    def display_board(self):
         self.clear_terminal()
         self.show_logo()
         self.display_players_score()
-        print(self._board.get_formatted_state())
+        print(self._board.get_formatted_state())  # Display formatted grid of cells
         print(f"\n{self._current_player.get_figure()}'s: {self._current_player.get_name()}")
         
-    def check_winning_combo_present(self) -> bool | None:
+    def round_winner_check(self) -> bool | None:
         for combo in self._board.winning_combinations():
             if self._board.cell_not_empty_check(*combo[0]):
                 cell_0 = self._board.get_cell_value(*combo[0])
                 cell_1 = self._board.get_cell_value(*combo[1])
                 cell_2 = self._board.get_cell_value(*combo[2])
                 if cell_0 == cell_1 and cell_1 == cell_2:
-                    self.result_victory()
+                    self.round_result_victory()
                     return True
 
-    def board_has_empty_cells(self):
+    def board_has_empty_cells_check(self):
         return True if self._board.get_empty_cells_count() >= 1 else False
-        
-    def report_round_winner(self,):
-        pass
-
-    def check_player_score(self, player):
-        if player.show_score() == self._score_limit:
-            return True
     
-    def check_for_winner(self):
+    def game_winner_check(self):
         if self._players[0] == self._players[1]:
             self._score_limit += 1
         elif self._current_player is not None:
@@ -130,23 +113,24 @@ class GameEngine:
                 return True
             
     def ask_for_user_input(self):
+        # User must enter cell address as column name and row number in order to make a move
         column_address = input('Enter column name (a, b c): ')
         while column_address not in self._board.get_state().keys():
-            self.show_board()
+            self.display_board()
             column_address = input('Invalid column name, please enter correct one  (E.g - a, b, c): ')
         row_address = input('Enter row number (1, 2, 3): ')
         while row_address not in self._board.get_state()['a'].keys():
-            self.show_board()
+            self.display_board()
             print(f'Selected column is "{column_address}"')
             row_address = input('Invalid row number, please enter correct one  (E.g - 1, 2, 3): ')
         return column_address, row_address
     
-    def result_draw(self):
+    def round_result_draw(self):
         self._board.empty()
         print("\nDraw!")
         input('\nPress "Enter" to continue: ')
     
-    def result_victory(self):
+    def round_result_victory(self):
         self._current_player.add_score()
         self._board.empty()
         print(f'\nPlayer {self._current_player.get_name()} is victorious!')
--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from game_engine import GameEngine
 
 def play_tic_tac_toe():
     engine = GameEngine()
-    engine.clear_terminal()
-    engine.show_logo()
-    engine.name_players()
-    engine.game_loop()
+    engine.clear_terminal()  # Clears everything from the console
+    engine.show_logo()  # Decorative logo at the top
+    engine.name_players()  # Ask users to enter their names
+    engine.game_loop()  # Run the main loop responsible for the game
 
 
 if __name__ == '__main__':
